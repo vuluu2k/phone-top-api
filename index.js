2c916c3deaf7e7e6378913289d78a189e5a81d7e
vuluu <tieungpk1@gmail.com>
2022-04-19 00:46:52 +0700

    deloy

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.use(
 
 app.use(cors());
 
+app.get('/pay', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Max-Age', '1800');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
+});
+
 route(app);
 
 app.listen(port, () => {
